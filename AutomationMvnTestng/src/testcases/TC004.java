93a88263a67394a3688873f5d3556cc05baf2fee
ksinghh <68163580+ksinghh@users.noreply.github.com>
2020-07-21 15:45:17 -0400

    TC004 is added

---
+++ AutomationMvnTestng/src/testcases/TC004.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC004 {
+	@Test
+	public static void meth() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhis\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+	
+		WebDriver driver=new ChromeDriver();
+		 
+		// Open Google
+		driver.get("http://www.google.com");
+		 
+		// Close browser
+		driver.close();	
+	}
+}
