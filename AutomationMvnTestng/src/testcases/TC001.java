ed921ba534ed5ff40288f7b461bdc8b8a28b0430
ksinghh <68163580+ksinghh@users.noreply.github.com>
2021-01-17 08:11:15 -0500

    TestCommit

--- AutomationMvnTestng/src/testcases/TC001.java
+++ AutomationMvnTestng/src/testcases/TC001.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 public class TC001 {
 	@Test
 	public static void meth() {
-	System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhis\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "C:/Users/abhis/eclipse-newworkspace/MySeleniumSept2020/drivers/ChromeDriver/chromedriver_win32 (6)/chromedriver.exe");
 	
 		WebDriver driver=new ChromeDriver();
 		 
